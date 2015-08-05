cbf7290f35b7247be4fe42569e902d8d47aa8365
noeliaaa <noeliapignatto@gmail.com>
2015-08-04 21:55:26 -0300

    tp final 1

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
+
+$('.carruselSlider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+});
  
+ 
+var titulo = document.getElementsByTagName("a");
+var imagenes=document.getElementsByTagName("img");
 
 
- 
+$(titulo).on("mouseenter", function() {
+			$(this).css("opacity", "0");
+			$(this).parent().find("img").css("opacity", '1');
+
+
+	});
+
+$(titulo).on("mouseleave", function() {
+			$(this).css("opacity", "1");
+			$(this).parent().find("img").css("opacity", '0.5');
+
+	});
+
+
+
+
 });
